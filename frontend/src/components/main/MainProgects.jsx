cde8ef71a74f56356a3a153cbf99d7cbecabf438
Alecaring <caringella.alessio1306@gmail.com>
2024-12-31 13:46:37 +0100

    add: project page with filters and example array

--- frontend/src/components/main/MainProgects.jsx
+++ frontend/src/components/main/MainProgects.jsx
@@ -1,11 +1,60 @@
-import SidebarProjects from "./sidebars/SidebarProjects"
+import SidebarProjects from "./sidebars/SidebarProjects";
+import "../../scss/partials/mainPorjects.scss";
+import { useState } from "react";
 
 function MainProgects() {
+    const [projectsData, setProjectsData] = useState([
+        {
+            id: 1,
+            projTitle: "project 1",
+            projSubtitle: "// _ui-animation",
+            progImg: "https://img.freepik.com/vettori-gratuito/sfondo-realistico-in-stile-futuristico_23-2149129125.jpg",
+            progIcon: "R",
+            progDescription: "Questa è la descrizione di questo fantastico progetto nato per salvare il mondo",
+            tags: ["React", "CSS"],
+        },
+        {
+            id: 2,
+            projTitle: "project 2",
+            projSubtitle: "// _web-development",
+            progImg: "https://img.freepik.com/vettori-gratuito/sfondo-colorato-geometrico-tecnologico_23-2149111277.jpg",
+            progIcon: "H",
+            progDescription: "Un progetto straordinario per l'innovazione tecnologica",
+            tags: ["HTML", "Vue"],
+        },
+    ]);
+
+    const [selectedFilters, setSelectedFilters] = useState([]); // Stato per i filtri selezionati
+
+    const filteredProjects = projectsData.filter(project =>
+        selectedFilters.length === 0 || project.tags.some(tag => selectedFilters.includes(tag))
+    );
+
     return (
         <>
-        <SidebarProjects />
+            <SidebarProjects setSelectedFilters={setSelectedFilters} />
+            <div className="main-projects-section">
+                {filteredProjects.map((s) => (
+                    <div key={s.id} className="card">
+                        <div className="card-header">
+                            <p>{s.projTitle}</p>
+                            <span>{s.projSubtitle}</span>
+                        </div>
+                        <div className="card-main">
+                            <div className="card-image-cont">
+                                <img src={s.progImg} alt="" />
+                                <span className="ref-icon">{s.progIcon}</span>
+                            </div>
+                            <div className="description">
+                                <p>{s.progDescription}</p>
+                                <button>view-project</button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </>
-    )
+    );
 }
 
 export default MainProgects;
--- frontend/src/components/main/sidebars/SidebarProjects.jsx
+++ frontend/src/components/main/sidebars/SidebarProjects.jsx
@@ -1,11 +1,29 @@
 import "../../../scss/partials/sidebars/sidebarPorjects.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
-import { faReact } from '@fortawesome/free-brands-svg-icons'; // Import dal pacchetto brand
+import { faReact, faHtml5, faCss3Alt, faVuejs } from '@fortawesome/free-brands-svg-icons'; 
+import { useState } from "react";
 
+function SidebarProjects({ setSelectedFilters }) {
+    const [list, setList] = useState([
+        { id: 1, icon: faReact, name: "React" },
+        { id: 2, icon: faHtml5, name: "HTML" },
+        { id: 3, icon: faCss3Alt, name: "CSS" },
+        { id: 4, icon: faVuejs, name: "Vue" },
+    ]);
 
+    const [checkedItems, setCheckedItems] = useState([]);
 
-function SidebarProjects() {
+    const handleCheckboxChange = (name) => {
+        setCheckedItems(prevState =>
+            prevState.includes(name) ? prevState.filter(item => item !== name) : [...prevState, name]
+        );
+    };
+
+    const handleSubmitFilter = () => {
+        setSelectedFilters(checkedItems); // Passo i filtri selezionati
+    };
+
     return (
         <div className="container-sidebar-projects">
             <div className="main-folder">
@@ -13,54 +31,21 @@ function SidebarProjects() {
                 <p>projects</p>
             </div>
             <ul>
-                <li className="list-item">
-                    <input type="checkbox" />
-                    <FontAwesomeIcon icon={faReact} />
-                    <p>React</p>
-                </li>
-                <li className="list-item">
-                    <input type="checkbox" />
-                    <FontAwesomeIcon icon={faReact} />
-                    <p>HTML</p>
-                </li>
-                <li className="list-item">
-                    <input type="checkbox" />
-                    <FontAwesomeIcon icon={faReact} />
-                    <p>CSS</p>
-                </li>
-                <li className="list-item">
-                    <input type="checkbox" />
-                    <FontAwesomeIcon icon={faReact} />
-                    <p>React</p>
-                </li>
-                <li className="list-item">
-                    <input type="checkbox" />
-                    <FontAwesomeIcon icon={faReact} />
-                    <p>HTML</p>
-                </li>
-                <li className="list-item">
-                    <input type="checkbox" />
-                    <FontAwesomeIcon icon={faReact} />
-                    <p>CSS</p>
-                </li>
-                <li className="list-item">
-                    <input type="checkbox" />
-                    <FontAwesomeIcon icon={faReact} />
-                    <p>React</p>
-                </li>
-                <li className="list-item">
-                    <input type="checkbox" />
-                    <FontAwesomeIcon icon={faReact} />
-                    <p>HTML</p>
-                </li>
-                <li className="list-item">
-                    <input type="checkbox" />
-                    <FontAwesomeIcon icon={faReact} />
-                    <p>CSS</p>
-                </li>
+                {list.map((s) => (
+                    <li key={s.id} className="list-item">
+                        <input
+                            type="checkbox"
+                            onChange={() => handleCheckboxChange(s.name)}
+                            checked={checkedItems.includes(s.name)}
+                        />
+                        <FontAwesomeIcon icon={s.icon} />
+                        <p>{s.name}</p>
+                    </li>
+                ))}
+                <button onClick={handleSubmitFilter} type="button">_filter</button>
             </ul>
         </div>
-    )
+    );
 }
 
 export default SidebarProjects;
