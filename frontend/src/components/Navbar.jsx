86c02ffaae50727d2d04a33bdba4afeaecf96466
Alecaring <caringella.alessio1306@gmail.com>
2025-01-07 10:56:45 +0100

    FRONTEND: add: dropdown menu and responsive layout for navbar and home component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,24 @@
 import { NavLink } from "react-router-dom";
 import "../scss/partials/navbar.scss";
 import { useAuth } from "../contexts/AuthContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
 
 function Navbar({ ownerName }) {
     const { isAuth } = useAuth();
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const openMenu = () => {
+        setIsMenuOpen(prevIsMenuOpen => !prevIsMenuOpen);
+    }
+    // useEffect(() => {
+
+
+    //     openMenu();
+
+    // }, [])
 
     return (
         <div className="container-navbar text-white">
@@ -54,6 +69,21 @@ function Navbar({ ownerName }) {
                         </li>
                     </ul>
                 </div>
+            </div>
+
+            {/* dispositivi diversi da desktop */}
+            <div className="container-hamburger-menu">
+                <FontAwesomeIcon icon={faBars} onClick={openMenu} />
+            </div>
+            <div className={ isMenuOpen ? "dropdown-menu-open" : "dropdown-menu" }>
+                <div className="container-exit-button-menu">
+                    <FontAwesomeIcon icon={faX} onClick={openMenu} />
+                </div>
+                <ul>
+                    <li>link</li>
+                    <li>link</li>
+                    <li>link</li>
+                </ul>
             </div>
         </div>
     );
